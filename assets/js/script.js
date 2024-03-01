741d7cdccc1757ea314c3690874fc03d7487a1fb
Alisson Seiti <alissonseiti@alunos.utfpr.edu.br>
2024-02-29 21:22:16 -0300

    Adicionando toastbox para copiar senha

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ function gerarSenha(incluirLetrasMinusculas, incluirLetrasMaiusculas, incluirNum
     caracteresPermitidos += incluirNumeros ? numeros : "";
     caracteresPermitidos += incluiCaracteresEspeciais ? caracteresEspeciais : "";
 
-    if(caracteresPermitidos.length !== 0){
+    if (caracteresPermitidos.length !== 0) {
         for (let i = 0; i < QuantidadeDeCaracteres; i++) {
             const indexAleatorio = Math.floor(Math.random() * caracteresPermitidos.length)
             senha += caracteresPermitidos[indexAleatorio]
@@ -27,19 +27,11 @@ function gerarSenha(incluirLetrasMinusculas, incluirLetrasMaiusculas, incluirNum
     return console.log(`Deve ser selecionado pelo menos 1 grupo de caracteres.`)
 }
 
-function copiarSenha() {
-    inputSenha.select()
-    navigator.clipboard.writeText(inputSenha.value).then(function() {
-        console.log("Texto copiado para a área de transferência");
-    }).catch(function(err) {
-        console.error("Erro ao copiar o texto para a área de transferência", err);
-    });
-}
-
 const botaoGerarSenha = document.querySelector("#botao-gerarSenha");
 const inputSenha = document.querySelector("#input-senha");
 const botaoCopiar = document.querySelector("#copy-icon")
 
+const toastBox = document.querySelector(".toastBox")
 
 const opcoes = document.querySelector(".opcoes")
 const botaoOpcoes = document.querySelector(".botao-opcoes");
@@ -53,6 +45,35 @@ const toggleCaracteresEspeciais = document.getElementById("check-4")
 const botaoTema = document.getElementById("tema");
 
 const botaoMostrarSenha = document.getElementById("visibility-icon");
+
+function copiarSenha() {
+    inputSenha.select()
+    navigator.clipboard.writeText(inputSenha.value).then(function () {
+        const mensagem = "Texto copiado para a área de transferência com sucesso"
+        const imgDone = document.createElement('img');
+        const imgDonePath = "assets/img/done_icon.svg";
+    
+        imgDone.src = imgDonePath;
+        mostrarToast(mensagem, imgDone);
+    }).catch(function (err) {
+        console.error();
+        const mensagem = `Erro ao copiar o texto para a área de transferência ${err}`
+        mostrarToast(mensagem);
+    });
+}
+
+function mostrarToast(mensagem, img) {
+    const toastDiv = document.createElement('div');
+
+    toastDiv.classList.add('toast');
+    img.classList.add('imgDone');
+    toastDiv.append(img, mensagem);
+    toastBox.appendChild(toastDiv);
+
+    setTimeout(()=> {
+        toastDiv.remove();
+    }, 2000);
+}
 
 botaoOpcoes.addEventListener("click", () => opcoes.classList.toggle("ativo"))
 
@@ -74,35 +95,35 @@ toggleCaracteresEspeciais.addEventListener("change", () => incluirCaracteresEspe
 botaoGerarSenha.addEventListener("click", () => gerarSenha(incluirLetrasMinusculas, incluirLetrasMaiusculas, incluirNumeros, incluirCaracteresEspeciais))
 botaoCopiar.addEventListener("click", copiarSenha)
 
-if(localStorage.getItem("tema") == null){
+if (localStorage.getItem("tema") == null) {
     localStorage.setItem("tema", "dark");
 }
 
 let temaLocalStorage = localStorage.getItem("tema")
 
-if(temaLocalStorage == "dark"){
+if (temaLocalStorage == "dark") {
     botaoTema.innerHTML = "light_mode"
-}else if (temaLocalStorage == "light"){
+} else if (temaLocalStorage == "light") {
     botaoTema.innerHTML = "dark_mode"
     document.body.classList.add("light-theme")
 }
 
-botaoTema.addEventListener("click", ()=> {
+botaoTema.addEventListener("click", () => {
     document.body.classList.toggle("light-theme")
-    if(document.body.classList.contains("light-theme")){
+    if (document.body.classList.contains("light-theme")) {
         botaoTema.innerHTML = "light_mode";
         localStorage.setItem("tema", "light");
-    }else{
+    } else {
         botaoTema.innerHTML = "dark_mode"
         localStorage.setItem("tema", "dark");
     }
 })
 
-botaoMostrarSenha.addEventListener("click", ()=> {
-    if(botaoMostrarSenha.innerHTML == "visibility"){
+botaoMostrarSenha.addEventListener("click", () => {
+    if (botaoMostrarSenha.innerHTML == "visibility") {
         botaoMostrarSenha.innerHTML = "visibility_off";
         inputSenha.type = "text";
-    }else{
+    } else {
         botaoMostrarSenha.innerHTML = "visibility";
         inputSenha.type = "password";
     }
